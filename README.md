70eea146a3130be161dd3cc01675b54396be23cb
Quantum-Legend <mahaleaditya2003@gmail.com>
2023-05-11 01:03:27 +0530

    This commit

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 A repository for _Data Structures and Algorithms Laboratory_ (fourth semester)
 
 ## Assignments:
-[x] [Group A - No. 1 (Inc)](#Group-A---No.-1)
-[ ] [Group A - No. 4](#Group-A---No.-4)
+* [Group A - No. 1 (Inc)](#Group-A---No.-1)
+* [Group A - No. 4](#Group-A---No.-4)
 
 
 ### Group A - No. 1
@@ -16,14 +16,20 @@ telephone numbers.
 ### Group A - No. 4
 
 To create ADT that implement the "set" concept.
+
 a. Add (newElement) -Place a value into the set
+
 b. Remove (element) Remove the value
+
 c. Contains (element) Return true if element is in collection
+
 d. Size () Return number of values in collection Iterator () Return an iterator used to loop
 over collection
+
 e. Intersection of two sets
+
 f. Union of two sets
+
 g. Difference between two sets
+
 h. Subset
-
-
--- telephone_hashtable_A1.cpp
+++ telephone_hashtable_A1.cpp
@@ -27,6 +27,7 @@ class hash
     for(int i = 0; i < size; i++)
     {
       flag[i] = 0;
+      hashtable[i].telephoneNo = -1;
     }
   }
   hash(int a)
@@ -38,26 +39,140 @@ class hash
     for(int i = 0; i < size; i++)
     {
       flag[i] = 0;
+      hashtable[i].telephoneNo = -1;
     }
   }
-  void insert();
+  void insertLinearProbing(std::string name, long telephoneNo);
+  void insertQuadraticProbing(std::string name, long telephoneNo);
+  int searchLinearProbing(std::string name);
+  int searchQuadraticProbing(std::string name);
   void display();
 };
 
-void hash::insert()
+void hash::insertLinearProbing(std::string name, long telephoneNo)
 {
   telephoneRecord *t = new telephoneRecord;
-  std::cout << "Enter name" << std::endl;
-  std::cin >> t->name;
-  std::cout << "Enter telephone number" << std::endl;
-  std::cin >> t->telephoneNo;
+  t->name = name;
+  t->telephoneNo = telephoneNo;
+
+  int loc, ASCII_sum = 0;
+  for(int i = 0; i < t->name.size(); i++)
+  {
+    ASCII_sum = ASCII_sum + int(t->name[i]);
+  }
+  loc = (ASCII_sum/t->name.size()) % size;
   for(int i = 0; i < size; i++)
   {
-    if(flag[i] == 0)
+    if(flag[loc] == 0)
     {
-      hashtable[i] = *t;
-      flag[i] = 1;
+      hashtable[loc] = *t;
+      flag[loc] = 1;
       break;
+    }
+    else
+    {
+      loc = (loc + 1) % size;
+    }
+  }
+}
+
+void hash::insertQuadraticProbing(std::string name, long telephoneNo)
+{
+  telephoneRecord *t = new telephoneRecord;
+  t->name = name;
+  t->telephoneNo = telephoneNo;
+
+  int loc, ASCII_sum = 0;
+  for(int i = 0; i < t->name.size(); i++)
+  {
+    ASCII_sum = ASCII_sum + int(t->name[i]);
+  }
+  loc = (ASCII_sum/t->name.size()) % size;
+  for(int i = 0; i < size; i++)
+  {
+    if(flag[loc] == 0)
+    {
+      hashtable[loc] = *t;
+      flag[loc] = 1;
+      break;
+    }
+    else
+    {
+      loc = (loc + i*i) % size;
+    }
+  }
+}
+
+int hash::searchLinearProbing(std::string name)
+{
+  int loc, ASCII_sum = 0, count = 0;
+  for(int i =0; i < name.size(); i++)
+  {
+    ASCII_sum = ASCII_sum + int(name[i]);
+  }
+  loc = (ASCII_sum/name.size()) % size;
+
+  if(flag[loc] == 0)
+  {
+    std::cout << name << " does not exist in the telephone book!" << std::endl;
+    return -1;
+  }
+  else
+  {
+    if(hashtable[loc].name == name)
+    {
+      std::cout << "The phone number of " << name << "is " << hashtable[loc].telephoneNo << std::endl;
+      return count;
+    }
+    else
+    {
+      for(int i = 0; i < size; i++)
+      {
+        loc = (loc + 1) % size;
+        count++;
+        if(hashtable[loc].name == name)
+        {
+          std::cout << "The phone number of " << name << "is " << hashtable[loc].telephoneNo << std::endl;
+          return count;
+        }
+      }
+    }
+  }
+}
+
+int hash::searchQuadraticProbing(std::string name)
+{
+  int loc, ASCII_sum = 0, count = 0;
+  for(int i =0; i < name.size(); i++)
+  {
+    ASCII_sum = ASCII_sum + int(name[i]);
+  }
+  loc = (ASCII_sum/name.size()) % size;
+
+  if(flag[loc] == 0)
+  {
+    std::cout << name << " does not exist in the telephone book!" << std::endl;
+    return -1;
+  }
+  else
+  {
+    if(hashtable[loc].name == name)
+    {
+      std::cout << "The phone number of " << name << "is " << hashtable[loc].telephoneNo << std::endl;
+      return count;
+    }
+    else
+    {
+      for(int i = 0; i < size; i++)
+      {
+        loc = (loc + i*i) % size;
+        count++;
+        if(hashtable[loc].name == name)
+        {
+          std::cout << "The phone number of " << name << "is " << hashtable[loc].telephoneNo << std::endl;
+          return count;
+        }
+      }
     }
   }
 }
@@ -67,27 +182,55 @@ void hash::display()
   telephoneRecord *t;
   for(int i = 0; i < size; i++)
   {
-    if(flag[i] == 1)
-    {
       t = &hashtable[i];
       std::cout << t->name << " - " << t->telephoneNo << std::endl;
-    }
   }
 }
 
 int main()
-{
-  hash book1, book2(5);
-  book1.insert();
-  book1.insert();
-  book1.insert();
-  book1.insert();
-  book1.display();
-  book2.insert();
-  book2.insert();
-  book2.insert();
-  book2.insert();
-  book2.insert();
-  book2.display();
+{ 
+  int ch, n1, n2;
+  std::string name;
+  long telephoneNo;
+  hash book1, book2;
+
+  do{
+    std::cout << "1) Insert \n2) Search \n3) Display \n4) Exit" << std::endl;
+    std::cin >> ch;
+
+    switch(ch){
+    case 1:
+      std::cout << "Enter name" << std::endl;
+      std::getline(std::cin >> std::ws, name);
+      std::cout << "Enter telephone number" << std::endl;
+      std::cin >> telephoneNo;
+      book1.insertLinearProbing(name, telephoneNo);
+      book2.insertQuadraticProbing(name, telephoneNo);
+      break;
+
+    case 2:
+      std::cout << "Enter name to be searched: ";
+      std::getline(std::cin >> std::ws, name);
+      n1 = book1.searchLinearProbing(name);
+      n2 = book2.searchQuadraticProbing(name);
+      std::cout << "No. of comparisons by linear probing: " << n1 << std::endl;
+      std::cout << "No. of comparisons by quadratic probing: " << n2 << std::endl;
+      break;
+
+    case 3:
+      std::cout << "Book 1 (Linear Probing): " << std::endl;
+      book1.display();
+      std::cout << std::endl;
+      std::cout << "Book 2 (Quadratic Probing): " << std::endl;
+      book2.display();
+      break;
+
+    default:
+      std::cout << "Please enter valid choice" << std::endl;
+      break;
+    }
+
+}while(ch!=4);
   return 0;
+
 }
